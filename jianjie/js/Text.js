743c772426466edbcc9ebfcd3d11ecb64daa3686
sxdwdwd <15584664617sun@gmail.com>
2025-03-16 21:38:46 +0800

    finish

--- jianjie/js/Text.js
+++ jianjie/js/Text.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var popupText = document.getElementById("popupText");
 
   // 定义要显示的文字列表
-  var texts = ["📢：主页内容已更新","🔔：右键或长按后点击模式可切换风格","🔔：感谢大家收藏😃","🔔：如果觉得有帮助，欢迎分享😎","🔔：感谢大家赞助🥳"];
+  var texts = ["📢：简洁模式主要是聚合资源","🔔：右键或长按后点击模式可切换风格","🔔：感谢大家收藏😃","🔔：如果觉得有帮助，欢迎分享😎","🔔：感谢大家赞助🥳"];
   var currentIndex = 0;
 
   // 更新弹出框文字内容
